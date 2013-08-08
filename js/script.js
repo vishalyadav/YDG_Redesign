6ef04703830bf70dce4529c5398a4b7316c2c0e4
vishal <vishal_s_yadav@yahoo.com>
2013-08-07 17:31:26 -0700

    Added home page navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	$("#header").localScroll();
+	$("#circle_nav").localScroll();
+	/*$(".circle").hover(function(){
+		alert("butts");
+		$(this).animate({"background-color": "#FFFFFF" }, 400);
+	}, function(){
+		$(this).animate({"background-color": "#AAAAAA" }, 400);
+	});*/
 });
