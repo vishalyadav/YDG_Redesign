cdf1adafb62de60d31d2098fe45dcb3e251f9908
vishal <vishal_s_yadav@yahoo.com>
2013-08-14 14:57:33 -0700

    Added about and services sections.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
 $(document).ready(function(){
 	$("#header").localScroll();
 	$("#circle_nav").localScroll();
-	/*$(".circle").hover(function(){
-		alert("butts");
-		$(this).animate({"background-color": "#FFFFFF" }, 400);
-	}, function(){
-		$(this).animate({"background-color": "#AAAAAA" }, 400);
-	});*/
+	window.location.replace("#home");
+	$(window).scrollTop(0);
+	if($(window).width() <= 840) {
+			if($("#home").hasClass("inview")) {
+				$("#home").css({"background-position":"0 " + 70 + "px" });
+				$("#home").css({"background-size": "840px auto"});
+			}
+	}
+
+	$("#home, #about, #services").bind("inview", function (event, visible) {
+			if (visible == true) {
+				$(this).addClass("inview");
+			} else {
+				$(this).removeClass("inview");
+			}
+	});
+
+	$(window).bind('scroll', function(){
+	    var position = $(window).scrollTop();
+		if($("#home").hasClass("inview")) {
+			if($(window).width() > 840) {
+				$("#home").css({"background-position":"50% " + (70 - (position * 0.25)) + "px" });
+			}
+		}
+		if($("#about").hasClass("inview")) {
+			if($(window).width() > 840) {
+				$("#about").css({"background-position":"50% " + (240 - (position * 0.25)) + "px" });
+			}
+		}
+		if($("#services").hasClass("inview")) {
+			if($(window).width() > 840) {
+				$("#services").css({"background-position":"50% " + (410 - (position * 0.25)) + "px" });
+			}
+		}
+	});
+
+	$(window).resize(function(){
+		if($(window).width() <= 840) {
+			if($("#home").hasClass("inview")) {
+				$("#home").css({"background-position":"0 " + 70 + "px" });
+				$("#home").css({"background-size": "840px auto"});
+			}
+		} else {
+			if($("#home").hasClass("inview")) {
+				$("#home").css({"background-position":"50% " + (70 - ($(window).scrollTop() * 0.25)) + "px" });
+				$("#home").css({"background-size": "100% auto"});
+			}
+		}
+	});
 });
